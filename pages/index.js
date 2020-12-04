d80c0f66935701d6fd576ec50f871f142f1096c3
Richard <digitalsadhu@gmail.com>
2020-12-04 22:34:09 +0100

    change page title

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home(props) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Waterdeep</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
